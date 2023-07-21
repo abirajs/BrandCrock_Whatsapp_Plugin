09d1ac440fbc7f00534b79552fc8629e16078a44
abirajs <novalnetchennai@gmail.com>
2023-07-21 17:37:00 +0530

    Update Settings.php

--- src/Models/Settings.php
+++ src/Models/Settings.php
@@ -41,7 +41,7 @@ class Settings extends Model
      */
     public function getTableName(): string
     {
-        return 'BrandCrock::settings';
+        return 'BrandCrockWhatsapp::settings';
     }
 
     /**
